c3ae768b8fb2bde808204015201c947963e2c250
Wessel de Vries <wesl.devries@gmail.com>
2021-05-10 15:09:25 +0200

    Complete code of wordpicker

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const generateRandomNumber = () => {
-  const randomNumber = Math.floor(Math.random()*5);
+const loveStatus = ['drama', 'success', 'surprises', 'struggles'];
+const moneyStatus = ['great', 'fair', 'mediocre', 'bad'];
+const consider = ['outfit', 'friends', 'hobbies', 'work'];
+
+const generateRandomNumber = (array) => {
+  const randomNumber = Math.floor(Math.random()*array.length);
   return randomNumber;
 };
 
-//Thanks Flo
-//Whatever Flo
+const pickLove = () => {
+  return loveStatus[generateRandomNumber(loveStatus)];
+};
+
+const pickMoney = () => {
+  return moneyStatus[generateRandomNumber(moneyStatus)];
+};
+
+const pickConsideration = () => {
+  return consider[generateRandomNumber(consider)];
+};
+
+const generateMessage = () => {
+  const loveLife = pickLove();
+  const moneyLife = pickMoney();
+  const consideration = pickConsideration();
+  console.log(`Today your love life will be characterised by ${loveLife}.`);
+  console.log(`Financially it will be a ${moneyLife} day for you.`);
+  console.log(`Consider paying extra attention to your ${consideration}!`);
+};
+
+generateMessage();
