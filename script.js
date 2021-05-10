2db8f9ce57e9e052a83d4fc1a370801065ed6ed9
Wessel de Vries <wesl.devries@gmail.com>
2021-05-10 15:24:26 +0200

    shorter word pick code

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 const loveStatus = ['drama', 'success', 'surprises', 'struggles'];
 const moneyStatus = ['great', 'fair', 'mediocre', 'bad'];
-const consider = ['outfit', 'friends', 'hobbies', 'work'];
+const considerThings = ['outfit', 'friends', 'hobbies', 'work'];
 
 const generateRandomNumber = (array) => {
   const randomNumber = Math.floor(Math.random()*array.length);
   return randomNumber;
 };
 
-const pickLove = () => {
-  return loveStatus[generateRandomNumber(loveStatus)];
-};
-
-const pickMoney = () => {
-  return moneyStatus[generateRandomNumber(moneyStatus)];
-};
-
-const pickConsideration = () => {
-  return consider[generateRandomNumber(consider)];
-};
+const pickWordFromArray = (array) => {
+  return array[generateRandomNumber(array)];
+}
 
 const generateMessage = () => {
-  const loveLife = pickLove();
-  const moneyLife = pickMoney();
-  const consideration = pickConsideration();
+  const loveLife = pickWordFromArray(loveStatus);
+  const moneyLife = pickWordFromArray(moneyStatus);
+  const consideration = pickWordFromArray(considerThings);
   console.log(`Today your love life will be characterised by ${loveLife}.`);
   console.log(`Financially it will be a ${moneyLife} day for you.`);
   console.log(`Consider paying extra attention to your ${consideration}!`);
