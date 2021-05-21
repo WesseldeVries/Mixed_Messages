d03171de8569f80e7b494a2b799ab87b94ed481d
Wessel de Vries <wesl.devries@gmail.com>
2021-05-21 18:21:56 +0200

    simplify code and add random words

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const loveStatus = ['drama', 'success', 'surprises', 'struggles', 'joy', 'connection', 'attraction', 'security'];
-const moneyStatus = ['great', 'fair', 'mediocre', 'bad'];
-const considerThings = ['outfit', 'friends', 'hobbies', 'work'];
+const moneyStatus = ['great', 'fair', 'mediocre', 'bad', 'rough', 'superb', 'special'];
+const considerThings = ['outfit', 'friends', 'hobbies', 'work', 'pet', 'plants', 'telephone', 'physique'];
 
 const generateRandomNumber = (array) => {
   const randomNumber = Math.floor(Math.random()*array.length);
@@ -12,12 +12,9 @@ const pickWordFromArray = (array) => {
 }
 
 const generateMessage = () => {
-  const loveLife = pickWordFromArray(loveStatus);
-  const moneyLife = pickWordFromArray(moneyStatus);
-  const consideration = pickWordFromArray(considerThings);
-  const loveMessage = `Today your love life will be characterised by ${loveLife}.`;
-  const moneyMessage = `Financially it will be a ${moneyLife} day for you.`;
-  const considerMessage = `Consider paying extra attention to your ${consideration}!`;
+  const loveMessage = `Today your love life will be characterised by ${pickWordFromArray(loveStatus)}.`;
+  const moneyMessage = `Financially it will be a ${pickWordFromArray(moneyStatus)} day for you.`;
+  const considerMessage = `Consider paying extra attention to your ${pickWordFromArray(considerThings)}!`;
 
   document.getElementById('love').innerHTML = loveMessage;
   document.getElementById('money').innerHTML = moneyMessage;
